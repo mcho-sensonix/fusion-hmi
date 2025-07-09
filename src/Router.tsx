9e314ad5f88d681aec207341361eff69afc0ae08
mcho-sensonix <133242711+mcho-sensonix@users.noreply.github.com>
2025-07-09 16:48:48 -0500

    fix navlink, bigger buttons

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,12 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import { HomePage } from './pages/HomePage';
 import {HomePage} from './pages/HomePage';
 import {ModulesPage} from "./pages/Modules.tsx";
-
-const router = createBrowserRouter([
- {
-  path: '/',
-  element: <HomePage />,
- },
- {path:'/modules', element: <ModulesPage/>}
-]);
+import {  Route, Routes, Link } from 'react-router-dom';
 
 export function Router() {
- return <RouterProvider router={router} />;
+ return (
+   <Routes>
+     <Route path="/" element={<HomePage />} />
+     <Route path="/modules" element={<ModulesPage />} />
+   </Routes>
+ )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {StrictMode} from "react";
+import {BrowserRouter} from "react-router-dom";
+import {MantineProvider} from "@mantine/core";
+import {theme} from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-   <App />
+   <MantineProvider theme={theme}>
+   <BrowserRouter>
+     <App/>
+   </BrowserRouter>
+  </MantineProvider>
   </StrictMode>
 );
