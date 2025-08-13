5c3cb3e088554eea36888c4b77d866dfae645b46
mcho-sensonix <133242711+mcho-sensonix@users.noreply.github.com>
2025-08-13 13:04:47 -0500

    allow graphql endpoint to be set by env var

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,8 @@ export default defineConfig({
  server: {
   proxy: {
    '/api': {
-    target: 'http://localhost:7772/graphql',
+    target: process.env.SIGNALS_GRAPHQL_ENDPONINT ?? 'http://localhost:7772/graphql',
     headers: {"perms": "{\"root_group\": \"/a-trak-ou56/\",\"account_id\": \"3\",\"scope\": \"read write\"}"}
-    // changeOrigin: true,
-    // secure:false,
-    // rewrite: (path)=> path.replace('/api','')
    }
   }
  }
